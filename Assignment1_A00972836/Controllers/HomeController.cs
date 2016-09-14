63b5a53ced1d3c03a467f7e61d157a4486462818
A00972836 <A00972836@SE12L0301-07>
2016-09-13 21:29:09 -0700

    all bug fixes

---
+++ Assignment1_A00972836/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment1_Model_A00972836;
+using System.Data.Entity;
+
+namespace Assignment1_A00972836.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            NorthwindEntities ctx = new NorthwindEntities();
+            //DbSet<Region> regions = ctx.Regions;
+            IEnumerable<Region> regionList = (from c in ctx.Regions select c);
+
+            return View(regionList);
+        }
+    }
+}
